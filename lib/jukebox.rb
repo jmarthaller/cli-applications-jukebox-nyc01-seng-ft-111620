816e8ce884df4d0676c4ca8f0f27d43e38aaf263
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-03 19:06:27 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -151,9 +151,21 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  
+  if input == 1
+    puts "Playing #{songs[0]}"
+  end
 end
 
-
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
 
 
